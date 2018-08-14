bdc5b6861b0aab3d42d64401388e8410dfbd2cc7
mrbotline <gilamanusia9@gmail.com>
2018-08-14 19:33:11 +0800

    first commit

---
+++ falak.sh
@@ -0,0 +1,121 @@
+#! /data/data/com.termux/files/usr/bin/bash
+clear
+cd data
+
+echo "\033[92;1mtools wibu falka ft Mr.BOTLINE"
+echo "\033[32;1mthanks to team : W3LL SQUAD and Banten Cyber Team"
+echo "\033[34;1mWibu Is My Life"
+
+figlet Well Squad
+toilet Wibu
+
+echo "\033[33;1m1.>install tools Mr.BOTLINE"
+echo "\033[35;1m2.>install tools Sapammer sms"
+echo "\033[36;1m3.>liat blog Falka"
+echo "4.>install tools Mr.BAJINGAN"
+echo "5.>kumpulan web vuln com fabrik beras"
+echo "6.>cara deface Attachment Upload Repository"
+echo "7.cara deface Roxy Fileman File Upload "
+echo "8.>cara deface FCKeditor "
+echo "9.>cara deface Drupall Hiden Uploader"
+echo "10.>cara deface file manager"
+echo "\033[36;1m99.>bosen disini?"
+read -p "pilih sayang××>" well;
+
+if [ $well = 1 ] || [ $well = 01 ]
+then
+clear
+figlet Mr.BOTLINE gans
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/mrbotline/traitor
+cd Traitor
+sh Traitor
+fi
+
+if [ $well = 2 ] || [ $well = 02 ]
+then
+clear
+pkg update
+pkg upgrade
+git clone https://github.com/not404foundcyberteam/spam-mantan.git
+cd spam-mantan
+sh N4F1.sh
+fi
+
+if [ $well = 3 ] || [ $well = 03 ]
+then
+clear
+apt-get update
+apt-get upgrade
+pkg install w3m
+w3m http://www.falka15cry.ml
+fi
+
+if [ $well = 4 ] || [ $well = 04 ]
+then
+clear
+echo "\033[32;1m user name ==> BAJINGAN
+PASSWD ==> Gans"
+pkg update
+pkg upgrade
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $well = 5 ] || [ $well = 05 ]
+then
+echo "http://www.zambianacmisonline.org
+http://ticketexchange.co.il
+https://res.torontocentre.org
+http://m.nysdrillteams.com
+https://nnpessoa.com.br/site
+https://www.larimerhumane.org"
+fi
+
+if [ $well = 6 ] || [ $well = 06 ]
+then
+clear
+cat 1.txt
+clear
+fi
+
+if [ $well = 7 ] || [ $well = 07 ]
+then
+clear
+cat 2.txt
+clear
+fi
+
+ if [ $well = 8 ] || [ $well = 08 ]
+then
+clear
+cat 3.txt
+clear
+fi
+
+if [ $well = 9 ] || [ $well = 09 ]
+then
+clear
+cat 4.txt
+clear
+fi
+
+if [ $well = 10 ] || [ $well = 010 ]
+then
+clear
+cat 5.txt
+clear
+fi
+
+if [ $well = 99 ] || [ $well = 099 ]
+then
+clear
+echo "\033[36;1mselamat tinggal"
+echo " kembali lagi cintaku "
+sleep 1
+exit
+fi
